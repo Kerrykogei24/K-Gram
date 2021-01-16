4bfd9b50405ba466a5d5f48c880d17cbcabda845
Kerrykogei24 <kerrykomar.gmail.com>
2021-01-16 23:44:52 +0300

    created an image model

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    image= models.ImageField('image')
+    name= models.CharField(max_length=30)
+    caption= models.TextField()
+    comments = models.CharField(max_length=30,blank=True)
+    likes=models.ManyToManyField(User, related_name='blog_posts')
+    pub_date=models.DateTimeField(auto_now_add=True)
+
