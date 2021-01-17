63bb8ba2df4944d5bba5d81634d84eb085f6d6bd
Kerrykogei24 <kerrykomar.gmail.com>
2021-01-18 01:12:34 +0300

    created comment form

--- instagram/forms.py
+++ instagram/forms.py
@@ -7,3 +7,8 @@ class EditProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('profile_pic','bio','username')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ['user','pic_id']
