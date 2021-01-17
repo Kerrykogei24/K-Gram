071bcea398f56ebd29c36aa9ab8092641c3a5bb6
Kerrykogei24 <kerrykomar.gmail.com>
2021-01-17 08:47:46 +0300

    create a view function for index

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Image, Profile, Follow, Comment
 
 # Create your views here.
+def index(request):
+    posts = Image.objects.all()
+    comments = Comment.objects.filter(image_id).all()
+
+    return render(request, 'index.html',{'posts':posts,'comments':comments}
