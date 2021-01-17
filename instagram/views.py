aea9f6dad6d7638669de83215970cc924442bc4f
Kerrykogei24 <kerrykomar.gmail.com>
2021-01-18 01:22:08 +0300

    create a sign up form

--- instagram/views.py
+++ instagram/views.py
@@ -8,6 +8,21 @@ def index(request):
   
 
     return render(request, 'index.html',{'posts':posts})
+def signUp(request):    
+    if request.method=='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            name=form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            send=welcome_email(name,email)
+            HttpResponseRedirect('timeline')
+
+    else:
+        form = SignUpForm()
+
+    return render(request,'registration/registration_form.html',{'form':form})
+
 
 @login_required(login_url = '/accounts/login/')
 def profile(request):
