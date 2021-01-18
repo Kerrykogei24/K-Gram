e05a11bf99fc2e10cc60c168092fa0226c8e6c04
Kerrykogei24 <kerrykomar.gmail.com>
2021-01-18 18:06:00 +0300

    testing function

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Profile,Image,Comments
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileTest(TestCase):
+    def setUp(self):
+
+        self.name = User(username = 'kerry', email ='kerrykomar@gmail.com')
+        self.name = Profile(user = self.name, user_id = 1, bio = 'joker', profile_pic = 'img.jpg')
+
+    def test_instance(self):
+        
+        self.assertTrue(isinstance(self.mercy,Profile))
--- instagram/urls.py
+++ instagram/urls.py
@@ -1,21 +1,19 @@
+from django.conf.urls import url,include
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url,include
 
-
-
-
-urlpatterns = [
-    url(r'^$',views.index, name = 'landing'),
-    url(r'^accounts/profile/$',views.profile,name='profile'),
+urlpatterns=[
+    url(r'^$',views.index,name = 'timeline'),
     url(r'^newpost/$',views.new_post,name='newpost'),
     url(r'^editprofile/$',views.edit_profile,name='editprofile'),
     url(r'^accounts/profile/$',views.profile,name='profile'),
     url(r'^singlepic/(\d+)',views.single_pic,name='singlepic'),
     url(r'^search/',views.search_results,name = 'search_results'),
     url(r'^logout/$',views.logout_request,name="logout"),
-    url(r'^comment/<int:id>/',views.comment,name='comment'),
+    path('comment/<int:id>/',views.comment,name='comment'),
+    
 ]
 
 if settings.DEBUG:
